a611efb7063e0257933f2c063242c225af6f5c01
Eric <30737196+TheMiracleMango@users.noreply.github.com>
2021-04-20 13:20:08 -0700

    Motion planner hard coded (buggy) (#15)
    * Motion planning node hard-coded

---
+++ motion_planning/include/motion_planning/motion_planning.h
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+#include "move_control/MoveControlSrv.h"
+#include <geometry_msgs/Pose.h>
+
+#include <vector>
+#include <memory>
+
+
+struct BaseEndEffectorPoses {
+  geometry_msgs::Pose base_pose;
+  std::vector<geometry_msgs::Pose> end_effector_poses;
+};
+
+
+class MotionPlanning {
+public:
+MotionPlanning(int argc, char **argv);
+~MotionPlanning() {
+  nh_.reset();
+}
+
+bool run();
+
+private:
+
+void init() {
+  control_clt_ = nh_->serviceClient<move_control::MoveControlSrv>("move_control");
+}
+
+int move(const BaseEndEffectorPoses &base_end_effector_poses);
+
+std::unique_ptr<ros::NodeHandle> nh_;
+ros::ServiceClient control_clt_;
+};
